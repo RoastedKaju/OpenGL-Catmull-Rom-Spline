d48d0e7658dc79131a392c9ee4199f13b2a7eafc
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-11-08 00:30:22 +0500

    Create common header

---
+++ src/common.h
@@ -0,0 +1,10 @@
+#ifndef SPLINES_COMMON_H
+#define SPLINES_COMMON_H
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <GLFW/glfw3.h>
+
+#endif // SPLINES_COMMON_H
